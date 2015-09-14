2e9cea23adf461d221c0fd4504dc1adc5c37d2ad
7shi <7shi@live.jp>
2015-09-14 15:10:43 +0900

    カーネルを読み込んで0xd0(movl)だけを抽出

--- src/vax/Main.java
+++ src/vax/Main.java
@@ -1,7 +1,35 @@
 package vax;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            byte[] text = java.nio.file.Files.readAllBytes(Paths.get("samples/unix.text"));
+            for (int i = 0; i < text.length;) {
+                int len = 1;
+                String mne = "???";
+                switch (Byte.toUnsignedInt(text[i])) {
+                    case 0xd0:
+                        mne = "movl";
+                        break;
+                    default:
+                        ++i;
+                        continue;
+                }
+                System.out.printf("%08x:", i);
+                for (int j = 0; j < len; ++j) {
+                    System.out.printf(" %02x", text[i + j]);
+                }
+                System.out.println("  " + mne);
+                i += len;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
