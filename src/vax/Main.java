909b067e1a92d9581f2c2f71d798fe6f4608c94a
7shi <7shi@live.jp>
2015-09-14 15:17:35 +0900

    fetch()

--- src/vax/Main.java
+++ src/vax/Main.java
@@ -7,29 +7,35 @@ import java.util.logging.Logger;
 
 public class Main {
 
+    static byte[] text;
+    static int pc;
+
+    static int fetch() {
+        return Byte.toUnsignedInt(text[pc++]);
+    }
+
     public static void main(String[] args) {
         try {
-            byte[] text = java.nio.file.Files.readAllBytes(Paths.get("samples/unix.text"));
-            for (int i = 0; i < text.length;) {
-                int len = 1;
-                String mne = "???";
-                switch (Byte.toUnsignedInt(text[i])) {
-                    case 0xd0:
-                        mne = "movl";
-                        break;
-                    default:
-                        ++i;
-                        continue;
-                }
-                System.out.printf("%08x:", i);
-                for (int j = 0; j < len; ++j) {
-                    System.out.printf(" %02x", text[i + j]);
-                }
-                System.out.println("  " + mne);
-                i += len;
-            }
+            text = java.nio.file.Files.readAllBytes(Paths.get("samples/unix.text"));
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        }
+        while (pc < text.length) {
+            int pc2 = pc;
+            String mne = "???";
+            switch (fetch()) {
+                case 0xd0:
+                    mne = "movl";
+                    break;
+                default:
+                    continue;
+            }
+            System.out.printf("%08x:", pc2);
+            for (; pc2 < pc; ++pc2) {
+                System.out.printf(" %02x", text[pc2]);
+            }
+            System.out.println("  " + mne);
         }
     }
 }
