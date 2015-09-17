1b26d74ac6468ae1a53d935568c89472d272d296
7shi <7shi@live.jp>
2015-09-17 23:16:27 +0900

    オペランドの型の調査に基づいて列挙型を生成

--- src/vax/Main.java
+++ src/vax/Main.java
@@ -141,34 +141,126 @@ enum VAXType {
     }
 }
 
+enum VAXOps {
+
+    HALT(0x00, ""), NOP(0x01, ""), REI(0x02, ""), BPT(0x03, ""),
+    RET(0x04, ""), RSB(0x05, ""), LDPCTX(0x06, ""), SVPCTX(0x07, ""),
+    CVTPS(0x08, "wbwb"), CVTSP(0x09, "wbwb"), INDEX(0x0a, "llllll"), CRC(0x0b, "blwb"),
+    PROBER(0x0c, "bwb"), PROBEW(0x0d, "bwb"), INSQUE(0x0e, "bb"), REMQUE(0x0f, "bl"),
+    BSBB(0x10, "1"), BRB(0x11, "1"), BNEQ(0x12, "1"), BEQL(0x13, "1"),
+    BGTR(0x14, "1"), BLEQ(0x15, "1"), JSB(0x16, "b"), JMP(0x17, "b"),
+    BGEQ(0x18, "1"), BLSS(0x19, "1"), BGTRU(0x1a, "1"), BLEQU(0x1b, "1"),
+    BVC(0x1c, "1"), BVS(0x1d, "1"), BCC(0x1e, "1"), BLSSU(0x1f, "1"),
+    ADDP4(0x20, "wbwb"), ADDP6(0x21, "wbwbwb"), SUBP4(0x22, "wbwb"), SUBP6(0x23, "wbwbwb"),
+    CVTPT(0x24, "wbbwb"), MULP(0x25, "wbwbwb"), CVTTP(0x26, "wbbwb"), DIVP(0x27, "wbwbwb"),
+    MOVC3(0x28, "wbb"), CMPC3(0x29, "wbb"), SCANC(0x2a, "wbbb"), SPANC(0x2b, "wbbb"),
+    MOVC5(0x2c, "wbbwb"), CMPC5(0x2d, "wbbwb"), MOVTC(0x2e, "wbbbwb"), MOVTUC(0x2f, "wbbbwb"),
+    BSBW(0x30, "2"), BRW(0x31, "2"), CVTWL(0x32, "wl"), CVTWB(0x33, "wb"),
+    MOVP(0x34, "wbb"), CMPP3(0x35, "wbb"), CVTPL(0x36, "wbl"), CMPP4(0x37, "wbwb"),
+    EDITPC(0x38, "wbbb"), MATCHC(0x39, "wbwb"), LOCC(0x3a, "bwb"), SKPC(0x3b, "bwb"),
+    MOVZWL(0x3c, "wl"), ACBW(0x3d, "www2"), MOVAW(0x3e, "wl"), PUSHAW(0x3f, "w"),
+    ADDF2(0x40, "ff"), ADDF3(0x41, "fff"), SUBF2(0x42, "ff"), SUBF3(0x43, "fff"),
+    MULF2(0x44, "ff"), MULF3(0x45, "fff"), DIVF2(0x46, "ff"), DIVF3(0x47, "fff"),
+    CVTFB(0x48, "fb"), CVTFW(0x49, "fw"), CVTFL(0x4a, "fl"), CVTRFL(0x4b, "fl"),
+    CVTBF(0x4c, "bf"), CVTWF(0x4d, "wf"), CVTLF(0x4e, "lf"), ACBF(0x4f, "fff2"),
+    MOVF(0x50, "ff"), CMPF(0x51, "ff"), MNEGF(0x52, "ff"), TSTF(0x53, "f"),
+    EMODF(0x54, "fbflf"), POLYF(0x55, "fwb"), CVTFD(0x56, "fd"),
+    ADAWI(0x58, "ww"),
+    INSQHI(0x5c, "bq"), INSQTI(0x5d, "bq"), REMQHI(0x5e, "ql"), REMQTI(0x5f, "ql"),
+    ADDD2(0x60, "dd"), ADDD3(0x61, "ddd"), SUBD2(0x62, "dd"), SUBD3(0x63, "ddd"),
+    MULD2(0x64, "dd"), MULD3(0x65, "ddd"), DIVD2(0x66, "dd"), DIVD3(0x67, "ddd"),
+    CVTDB(0x68, "db"), CVTDW(0x69, "dw"), CVTDL(0x6a, "dl"), CVTRDL(0x6b, "dl"),
+    CVTBD(0x6c, "bd"), CVTWD(0x6d, "wd"), CVTLD(0x6e, "ld"), ACBD(0x6f, "ddd2"),
+    MOVD(0x70, "dd"), CMPD(0x71, "dd"), MNEGD(0x72, "dd"), TSTD(0x73, "d"),
+    EMODD(0x74, "dbdld"), POLYD(0x75, "dwb"), CVTDF(0x76, "df"),
+    ASHL(0x78, "bll"), ASHQ(0x79, "bqq"), EMUL(0x7a, "lllq"), EDIV(0x7b, "lqll"),
+    CLRD(0x7c, "d"), MOVQ(0x7d, "qq"), MOVAQ(0x7e, "ql"), PUSHAQ(0x7f, "q"),
+    ADDB2(0x80, "bb"), ADDB3(0x81, "bbb"), SUBB2(0x82, "bb"), SUBB3(0x83, "bbb"),
+    MULB2(0x84, "bb"), MULB3(0x85, "bbb"), DIVB2(0x86, "bb"), DIVB3(0x87, "bbb"),
+    BISB2(0x88, "bb"), BISB3(0x89, "bbb"), BICB2(0x8a, "bb"), BICB3(0x8b, "bbb"),
+    XORB2(0x8c, "bb"), XORB3(0x8d, "bbb"), MNEGB(0x8e, "bb"), CASEB(0x8f, "bbb"),
+    MOVB(0x90, "bb"), CMPB(0x91, "bb"), MCOMB(0x92, "bb"), BITB(0x93, "bb"),
+    CLRB(0x94, "b"), TSTB(0x95, "b"), INCB(0x96, "b"), DECB(0x97, "b"),
+    CVTBL(0x98, "bl"), CVTBW(0x99, "bw"), MOVZBL(0x9a, "bl"), MOVZBW(0x9b, "bw"),
+    ROTL(0x9c, "bll"), ACBB(0x9d, "bbb2"), MOVAB(0x9e, "bl"), PUSHAB(0x9f, "b"),
+    ADDW2(0xa0, "ww"), ADDW3(0xa1, "www"), SUBW2(0xa2, "ww"), SUBW3(0xa3, "www"),
+    MULW2(0xa4, "ww"), MULW3(0xa5, "www"), DIVW2(0xa6, "ww"), DIVW3(0xa7, "www"),
+    BISW2(0xa8, "ww"), BISW3(0xa9, "www"), BICW2(0xaa, "ww"), BICW3(0xab, "www"),
+    XORW2(0xac, "ww"), XORW3(0xad, "www"), MNEGW(0xae, "ww"), CASEW(0xaf, "www"),
+    MOVW(0xb0, "ww"), CMPW(0xb1, "ww"), MCOMW(0xb2, "ww"), BITW(0xb3, "ww"),
+    CLRW(0xb4, "w"), TSTW(0xb5, "w"), INCW(0xb6, "w"), DECW(0xb7, "w"),
+    BISPSW(0xb8, "w"), BICPSW(0xb9, "w"), POPR(0xba, "w"), PUSHR(0xbb, "w"),
+    CHMK(0xbc, "w"), CHME(0xbd, "w"), CHMS(0xbe, "w"), CHMU(0xbf, "w"),
+    ADDL2(0xc0, "ll"), ADDL3(0xc1, "lll"), SUBL2(0xc2, "ll"), SUBL3(0xc3, "lll"),
+    MULL2(0xc4, "ll"), MULL3(0xc5, "lll"), DIVL2(0xc6, "ll"), DIVL3(0xc7, "lll"),
+    BISL2(0xc8, "ll"), BISL3(0xc9, "lll"), BICL2(0xca, "ll"), BICL3(0xcb, "lll"),
+    XORL2(0xcc, "ll"), XORL3(0xcd, "lll"), MNEGL(0xce, "ll"), CASEL(0xcf, "lll"),
+    MOVL(0xd0, "ll"), CMPL(0xd1, "ll"), MCOML(0xd2, "ll"), BITL(0xd3, "ll"),
+    CLRF(0xd4, "f"), TSTL(0xd5, "l"), INCL(0xd6, "l"), DECL(0xd7, "l"),
+    ADWC(0xd8, "ll"), SBWC(0xd9, "ll"), MTPR(0xda, "ll"), MFPR(0xdb, "ll"),
+    MOVPSL(0xdc, "l"), PUSHL(0xdd, "l"), MOVAL(0xde, "ll"), PUSHAL(0xdf, "l"),
+    BBS(0xe0, "lb1"), BBC(0xe1, "lb1"), BBSS(0xe2, "lb1"), BBCS(0xe3, "lb1"),
+    BBSC(0xe4, "lb1"), BBCC(0xe5, "lb1"), BBSSI(0xe6, "lb1"), BBCCI(0xe7, "lb1"),
+    BLBS(0xe8, "l1"), BLBC(0xe9, "l1"), FFS(0xea, "lbbl"), FFC(0xeb, "lbbl"),
+    CMPV(0xec, "lbbl"), CMPZV(0xed, "lbbl"), EXTV(0xee, "lbbl"), EXTZV(0xef, "lbbl"),
+    INSV(0xf0, "llbb"), ACBL(0xf1, "lll2"), AOBLSS(0xf2, "ll1"), AOBLEQ(0xf3, "ll1"),
+    SOBGEQ(0xf4, "l1"), SOBGTR(0xf5, "l1"), CVTLB(0xf6, "lb"), CVTLW(0xf7, "lw"),
+    ASHP(0xf8, "bwbbwb"), CVTLP(0xf9, "lwb"), CALLG(0xfa, "bb"), CALLS(0xfb, "lb"),
+    XFC(0xfc, ""),
+    CVTDH(0xfd32, "dh"), CVTGF(0xfd33, "gh"),
+    ADDG2(0xfd40, "gg"), ADDG3(0xfd41, "ggg"), SUBG2(0xfd42, "gg"), SUBG3(0xfd43, "ggg"),
+    MULG2(0xfd44, "gg"), MULG3(0xfd45, "ggg"), DIVG2(0xfd46, "gg"), DIVG3(0xfd47, "ggg"),
+    CVTGB(0xfd48, "gb"), CVTGW(0xfd49, "gw"), CVTGL(0xfd4a, "gl"), CVTRGL(0xfd4b, "gl"),
+    CVTBG(0xfd4c, "bg"), CVTWG(0xfd4d, "wg"), CVTLG(0xfd4e, "lg"), ACBG(0xfd4f, "ggg2"),
+    MOVG(0xfd50, "gg"), CMPG(0xfd51, "gg"), MNEGG(0xfd52, "gg"), TSTG(0xfd53, "g"),
+    EMODG(0xfd54, "gwglg"), POLYG(0xfd55, "gwb"), CVTGH(0xfd56, "gh"),
+    ADDH2(0xfd60, "hh"), ADDH3(0xfd61, "hhh"), SUBH2(0xfd62, "hh"), SUBH3(0xfd63, "hhh"),
+    MULH2(0xfd64, "hh"), MULH3(0xfd65, "hhh"), DIVH2(0xfd66, "hh"), DIVH3(0xfd67, "hhh"),
+    CVTHB(0xfd68, "hb"), CVTHW(0xfd69, "hw"), CVTHL(0xfd6a, "hl"), CVTRHL(0xfd6b, "hl"),
+    CVTBH(0xfd6c, "bh"), CVTWH(0xfd6d, "wh"), CVTLH(0xfd6e, "lh"), ACBH(0xfd6f, "hhh2"),
+    MOVH(0xfd70, "hh"), CMPH(0xfd71, "hh"), MNEGH(0xfd72, "hh"), TSTH(0xfd73, "h"),
+    EMODH(0xfd74, "hwhlh"), POLYH(0xfd75, "hwb"), CVTHG(0xfd76, "hg"),
+    CLRH(0xfd7c, "h"), MOVO(0xfd7d, "oo"), MOVAH(0xfd7e, "hl"), PUSHAH(0xfd7f, "h"),
+    CVTFH(0xfd98, "fh"), CVTFG(0xfd99, "fg"),
+    CVTHF(0xfdf6, "hf"), CVTHD(0xfdf7, "hd"),
+    BUGL(0xfffd, "l"), BUGW(0xfffe, "w");
+
+    public final int op;
+    public final String oprs;
+
+    private VAXOps(int op, String oprs) {
+        this.op = op;
+        this.oprs = oprs;
+    }
+}
+
 class VAXOp {
 
     private final String mne;
-    private final VAXType[] types;
+    private final int op;
+    private final VAXType[] oprs;
 
-    public VAXOp(String mne) {
-        this.mne = mne;
-        types = new VAXType[0];
-    }
-
-    public VAXOp(String mne, VAXType type, int count) {
-        this.mne = mne;
-        types = new VAXType[count];
-        for (int i = 0; i < count; ++i) {
-            types[i] = type;
+    public VAXOp(VAXOps op) {
+        this.mne = op.toString().toLowerCase();
+        this.op = op.op;
+        int len = op.oprs.length();
+        this.oprs = new VAXType[len];
+        for (int i = 0; i < len; ++i) {
+            this.oprs[i] = VAXType.fromSuffix(op.oprs.charAt(i));
         }
     }
 
-    public VAXOp(String mne, VAXType type, int count, VAXType rel) {
-        this(mne, type, count + 1);
-        types[count] = rel;
+    public VAXOp(int op) {
+        this.mne = String.format(".word 0x%x", op);
+        this.op = op;
+        this.oprs = new VAXType[0];
     }
 
     public String read(Disasm dis) {
         StringBuilder sb = new StringBuilder(mne);
-        for (int i = 0; i < types.length; ++i) {
+        for (int i = 0; i < oprs.length; ++i) {
             sb.append(i == 0 ? " " : ",");
-            VAXType t = types[i];
+            VAXType t = oprs[i];
             switch (t) {
                 case RELB:
                 case RELW: {
@@ -185,188 +277,28 @@ class VAXOp {
     }
 }
 
-class VAXOps {
+class Disasm extends Memory {
 
-    private static VAXOp op(String mne) {
-        return new VAXOp(mne);
-    }
-
-    private static VAXOp b(String mne, int c) {
-        return new VAXOp(mne, VAXType.BYTE, c);
-    }
-
-    private static VAXOp bw(String mne, int c) {
-        return new VAXOp(mne, VAXType.BYTE, c, VAXType.RELW);
-    }
-
-    private static VAXOp w(String mne, int c) {
-        return new VAXOp(mne, VAXType.WORD, c);
-    }
-
-    private static VAXOp ww(String mne, int c) {
-        return new VAXOp(mne, VAXType.WORD, c, VAXType.RELW);
-    }
-
-    private static VAXOp l(String mne, int c) {
-        return new VAXOp(mne, VAXType.LONG, c);
-    }
-
-    private static VAXOp lb(String mne, int c) {
-        return new VAXOp(mne, VAXType.LONG, c, VAXType.RELB);
-    }
-
-    private static VAXOp lw(String mne, int c) {
-        return new VAXOp(mne, VAXType.LONG, c, VAXType.RELW);
-    }
-
-    private static VAXOp q(String mne, int c) {
-        return new VAXOp(mne, VAXType.QWORD, c);
-    }
-
-    private static VAXOp o(String mne, int c) {
-        return new VAXOp(mne, VAXType.OWORD, c);
-    }
-
-    private static VAXOp f(String mne, int c) {
-        return new VAXOp(mne, VAXType.FFLOAT, c);
-    }
-
-    private static VAXOp fw(String mne, int c) {
-        return new VAXOp(mne, VAXType.FFLOAT, c, VAXType.RELW);
-    }
-
-    private static VAXOp d(String mne, int c) {
-        return new VAXOp(mne, VAXType.DFLOAT, c);
-    }
-
-    private static VAXOp dw(String mne, int c) {
-        return new VAXOp(mne, VAXType.DFLOAT, c, VAXType.RELW);
-    }
-
-    private static VAXOp g(String mne, int c) {
-        return new VAXOp(mne, VAXType.GFLOAT, c);
-    }
-
-    private static VAXOp gw(String mne, int c) {
-        return new VAXOp(mne, VAXType.GFLOAT, c, VAXType.RELW);
-    }
-
-    private static VAXOp h(String mne, int c) {
-        return new VAXOp(mne, VAXType.HFLOAT, c);
-    }
-
-    private static VAXOp hw(String mne, int c) {
-        return new VAXOp(mne, VAXType.HFLOAT, c, VAXType.RELW);
-    }
-
-    private static VAXOp brb(String mne) {
-        return new VAXOp(mne, VAXType.RELB, 1);
-    }
-
-    private static VAXOp brw(String mne) {
-        return new VAXOp(mne, VAXType.RELW, 1);
-    }
-
-    private static VAXOp cvt(String t) {
-        VAXType t1 = VAXType.fromSuffix(t.charAt(t.length() - 2));
-        VAXType t2 = VAXType.fromSuffix(t.charAt(t.length() - 1));
-        return new VAXOp("cvt" + t, t1, 1, t2);
-    }
-
-    private static VAXOp word(int w) {
-        return new VAXOp(String.format(".word 0x%x", w));
-    }
-
-    private static final VAXOp[][] ops = {
-        new VAXOp[]{
-            /* 00 */op("halt"), op("nop"), op("rei"), op("bpt"), op("ret"), op("rsb"), op("ldpctx"), op("svpctx"),
-            /* 08 */ w("cvtps", 4), w("cvtsp", 4), l("index", 6), b("crc", 4), b("prober", 3), b("probew", 3), b("insque", 2), b("remque", 2),
-            /* 10 */ brb("bsbb"), brb("brb"), brb("bneq"), brb("beql"), brb("bgtr"), brb("bleq"), b("jsb", 1), b("jmp", 1),
-            /* 18 */ brb("bgeq"), brb("blss"), brb("bgtru"), brb("blequ"), brb("bvc"), brb("bvs"), brb("bcc"), brb("blssu"),
-            /* 20 */ w("addp4", 4), w("addp6", 6), w("subp4", 4), w("subp6", 6), w("cvtpt", 5), w("mulp", 6), w("cvttp", 5), w("divp", 6),
-            /* 28 */ w("movc3", 3), w("cmpc3", 3), w("scanc", 4), w("spanc", 4), w("movc5", 5), w("cmpc5", 5), w("movtc", 6), w("movtuc", 6),
-            /* 30 */ brw("bsbw"), brw("brw"), cvt("wl"), cvt("wb"), w("movp", 3), w("cmpp3", 3), w("cvtpl", 3), w("cmpp4", 4),
-            /* 38 */ w("editpc", 4), w("matchc", 4), b("locc", 3), b("skpc", 3), w("movzwl", 2), ww("acbw", 3), w("movaw", 2), w("pushaw", 1),
-            /* 40 */ f("addf2", 2), f("addf3", 3), f("subf2", 2), f("subf3", 3), f("mulf2", 2), f("mulf3", 3), f("divf2", 2), f("divf3", 3),
-            /* 48 */ cvt("fb"), cvt("fw"), cvt("fl"), cvt("rfl"), cvt("bf"), cvt("wf"), cvt("lf"), fw("acbf", 3),
-            /* 50 */ f("movf", 2), f("cmpf", 2), f("mnegf", 2), f("tstf", 1), f("emodf", 5), f("polyf", 3), cvt("fd"), null,
-            /* 58 */ w("adawi", 2), null, null, null, b("insqhi", 2), b("insqti", 2), q("remqhi", 2), q("remqti", 2),
-            /* 60 */ d("addd2", 2), d("addd3", 3), d("subd2", 2), d("subd3", 3), d("muld2", 2), d("muld3", 3), d("divd2", 2), d("divd3", 3),
-            /* 68 */ cvt("db"), cvt("dw"), cvt("dl"), cvt("rdl"), cvt("bd"), cvt("wd"), cvt("ld"), dw("acbd", 3),
-            /* 70 */ d("movd", 2), d("cmpd", 2), d("mnegd", 2), d("tstd", 1), d("emodd", 5), d("polyd", 3), cvt("df"), null,
-            /* 78 */ b("ashl", 3), b("ashq", 3), l("emul", 4), l("ediv", 4), d("clrd", 1), q("movq", 2), q("movaq", 2), q("pushaq", 1),
-            /* 80 */ b("addb2", 2), b("addb3", 3), b("subb2", 2), b("subb3", 3), b("mulb2", 2), b("mulb3", 3), b("divb2", 2), b("divb3", 3),
-            /* 88 */ b("bisb2", 2), b("bisb3", 3), b("bicb2", 2), b("bicb3", 3), b("xorb2", 2), b("xorb3", 3), b("mnegb", 2), b("caseb", 3),
-            /* 90 */ b("movb", 2), b("cmpb", 2), b("mcomb", 2), b("bitb", 2), b("clrb", 1), b("tstb", 1), b("incb", 1), b("decb", 1),
-            /* 98 */ cvt("bl"), cvt("bw"), b("movzbl", 2), b("movzbw", 2), b("rotl", 3), bw("acbb", 3), b("movab", 2), b("pushab", 1),
-            /* a0 */ w("addw2", 2), w("addw3", 3), w("subw2", 2), w("subw3", 3), w("mulw2", 2), w("mulw3", 3), w("divw2", 2), w("divw3", 3),
-            /* a8 */ w("bisw2", 2), w("bisw3", 3), w("bicw2", 2), w("bicw3", 3), w("xorw2", 2), w("xorw3", 3), w("mnegw", 2), w("casew", 3),
-            /* b0 */ w("movw", 2), w("cmpw", 2), w("mcomw", 2), w("bitw", 2), w("clrw", 1), w("tstw", 1), w("incw", 1), w("decw", 1),
-            /* b8 */ w("bispsw", 1), w("bicpsw", 1), w("popr", 1), w("pushr", 1), w("chmk", 1), w("chme", 1), w("chms", 1), w("chmu", 1),
-            /* c0 */ l("addl2", 2), l("addl3", 3), l("subl2", 2), l("subl3", 3), l("mull2", 2), l("mull3", 3), l("divl2", 2), l("divl3", 3),
-            /* c8 */ l("bisl2", 2), l("bisl3", 3), l("bicl2", 2), l("bicl3", 3), l("xorl2", 2), l("xorl3", 3), l("mnegl", 2), l("casel", 3),
-            /* d0 */ l("movl", 2), l("cmpl", 2), l("mcoml", 2), l("bitl", 2), f("clrf", 1), l("tstl", 1), l("incl", 1), l("decl", 1),
-            /* d8 */ l("adwc", 2), l("sbwc", 2), l("mtpr", 2), l("mfpr", 2), l("movpsl", 1), l("pushl", 1), l("moval", 2), l("pushal", 1),
-            /* e0 */ lb("bbs", 2), lb("bbc", 2), lb("bbss", 2), lb("bbcs", 2), lb("bbsc", 2), lb("bbcc", 2), lb("bbssi", 2), lb("bbcci", 2),
-            /* e8 */ lb("blbs", 1), lb("blbc", 1), l("ffs", 4), l("ffc", 4), l("cmpv", 4), l("cmpzv", 4), l("extv", 4), l("extzv", 4),
-            /* f0 */ l("insv", 4), lw("acbl", 3), lb("aoblss", 2), lb("aobleq", 2), lb("sobgeq", 1), lb("sobgtr", 1), cvt("lb"), cvt("lw"),
-            /* f8 */ b("ashp", 6), l("cvtlp", 3), b("callg", 2), l("calls", 2), op("xfc"), null, null, null
-        }, new VAXOp[0x100], new VAXOp[0x100], new VAXOp[0x100]
-    };
+    private static final VAXOp[] ops = new VAXOp[0x10000];
 
     static {
-        System.arraycopy(new VAXOp[]{
-            /* fd40 */g("addg2", 2), g("addg3", 3), g("subg2", 2), g("subg3", 3), g("mulg2", 2), g("mulg3", 3), g("divg2", 2), g("divg3", 3),
-            /* fd48 */ cvt("gb"), cvt("gw"), cvt("gl"), cvt("rgl"), cvt("bg"), cvt("wg"), cvt("lg"), gw("acbg", 3),
-            /* fd50 */ g("movg", 2), g("cmpg", 2), g("mnegg", 2), g("tstg", 1), g("emodg", 5), g("polyg", 3), cvt("gh"), null,
-            /* fd58 */ null, null, null, null, null, null, null, null,
-            /* fd60 */ h("addh2", 2), h("addh3", 3), h("subh2", 2), h("subh3", 3), h("mulh2", 2), h("mulh3", 3), h("divh2", 2), h("divh3", 3),
-            /* fd68 */ cvt("hb"), cvt("hw"), cvt("hl"), cvt("rhl"), cvt("bh"), cvt("wh"), cvt("lh"), hw("acbh", 3),
-            /* fd70 */ h("movh", 2), h("cmph", 2), h("mnegh", 2), h("tsth", 1), h("emodh", 5), h("polyh", 3), cvt("hg"), null,
-            /* fd78 */ null, null, null, null, h("clrh", 1), o("movo", 2), h("movah", 2), h("pushah", 1)
-        }, 0, ops[1], 0x40, 0x40);
-
-        System.arraycopy(new VAXOp[]{/* fd32 */cvt("dh"), cvt("gf")}, 0, ops[1], 0x32, 2);
-        System.arraycopy(new VAXOp[]{/* fd98 */cvt("fh"), cvt("fg")}, 0, ops[1], 0x98, 2);
-        System.arraycopy(new VAXOp[]{/* fdf6 */cvt("hf"), cvt("hd")}, 0, ops[1], 0xf6, 2);
-        System.arraycopy(new VAXOp[]{/* fffd */l("bugl", 1), w("bugw", 1)}, 0, ops[3], 0xfd, 2);
-
-        for (int i = 1; i < 4; i++) {
-            for (int j = 0; j < 0x100; ++j) {
-                if (ops[i][j] == null) {
-                    ops[i][j] = word((0xfc + i) << 8 | j);
+        for (VAXOps op : VAXOps.values()) {
+            ops[op.op] = new VAXOp(op);
+        }
+        for (int i = 0; i < 0x100; ++i) {
+            if (ops[i] == null) {
+                for (int b = i << 8, be = b + 0x100; b < be; ++b) {
+                    if (ops[b] == null) {
+                        ops[b] = new VAXOp(b);
+                    }
                 }
             }
         }
     }
 
-    private static final VAXOp[] words = new VAXOp[0x10000];
-
-    public static VAXOp fetch(Disasm dis) {
-        int b = dis.fetch();
-        if (b >= 0xfd) {
-            return ops[b - 0xfc][dis.fetch()];
-        }
-        VAXOp op = ops[0][b];
-        if (op != null) {
-            return op;
-        }
-        int bb = b << 8 | dis.fetch();
-        if (words[bb] == null) {
-            words[bb] = word(bb);
-        }
-        return words[bb];
-    }
-}
-
-class Disasm extends Memory {
-
     private static final String[] REGS = {
         "r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7",
         "r8", "r9", "r10", "r11", "ap", "fp", "sp", "pc"
-    };
-    private static final VAXType[] atype = {
-        VAXType.BYTE, VAXType.WORD, VAXType.LONG
     };
 
     public Disasm(String path) throws IOException {
@@ -409,7 +341,7 @@ class Disasm extends Memory {
             case 0xe:
             case 0xf: {
                 String prefix = (b1 & 1) == 1 ? "*" : "";
-                int disp = fetchSigned(atype[(b1 - 0xa) >> 1]);
+                int disp = fetchSigned(VAXType.values()[(b1 - 0xa) >> 1]);
                 if (b2 == 15) {
                     return String.format("%s0x%x", prefix, pc + disp);
                 } else {
@@ -421,15 +353,19 @@ class Disasm extends Memory {
         }
     }
 
-    String disasm1() {
-        return VAXOps.fetch(this).read(this);
+    public VAXOp disasm1() {
+        int b = fetch();
+        if (b < 0xfd && ops[b] != null) {
+            return ops[b];
+        }
+        return ops[b << 8 | fetch()];
     }
 
     void disasm(PrintStream out) {
         out.println(path);
         while (pc < text.length) {
             int oldpc = pc;
-            String asm = disasm1();
+            String asm = disasm1().read(this);
             output(out, oldpc, pc - oldpc, asm);
         }
     }
--- src/vax/tools/OpsGen.java
+++ src/vax/tools/OpsGen.java
@@ -6,144 +6,30 @@ import java.io.FileReader;
 
 public class OpsGen {
 
-    static String convert(String line, int p1, int b) {
-        int p2 = line.indexOf(' ', p1);
-        if (p2 < 0) {
-            return String.format("op(\"%s\")", line.substring(p1));
-        }
-        String mne = "\"" + line.substring(p1, p2) + "\"";
-        switch (line.substring(p2 + 1)) {
-            case "$0x50":
-                return String.format("b(%s, 1)", mne);
-            case "$0x50,r1":
-                return String.format("b(%s, 2)", mne);
-            case "$0x50,r1,r2":
-                return String.format("b(%s, 3)", mne);
-            case "$0x50,r1,r2,0x545a":
-                return String.format("bw(%s, 3)", mne);
-            case "$0x50,r1,r2,r3":
-                return String.format("b(%s, 4)", mne);
-            case "$0x50,r1,r2,r3,r4,r5":
-                return String.format("b(%s, 6)", mne);
-            case "$0x5150":
-                return String.format("w(%s, 1)", mne);
-            case "$0x5150,r2":
-                return String.format("w(%s, 2)", mne);
-            case "$0x5150,r2,r3":
-                return String.format("w(%s, 3)", mne);
-            case "$0x5150,r2,r3,0x555c":
-                return String.format("ww(%s, 3)", mne);
-            case "$0x5150,r2,r3,r4":
-                return String.format("w(%s, 4)", mne);
-            case "$0x5150,r2,r3,r4,r5":
-                return String.format("w(%s, 5)", mne);
-            case "$0x5150,r2,r3,r4,r5,r6":
-                return String.format("w(%s, 6)", mne);
-            case "$0x53525150":
-                return String.format("l(%s, 1)", mne);
-            case "$0x53525150 [f-float]":
-                return String.format("f(%s, 1)", mne);
-            case "$0x53525150 [f-float],r4":
-                return String.format("f(%s, 2)", mne);
-            case "$0x53525150 [f-float],r4,r5":
-                return String.format("f(%s, 3)", mne);
-            case "$0x53525150 [f-float],r4,r5,0x5760":
-                return String.format("fw(%s, 3)", mne);
-            case "$0x53525150 [f-float],r4,r5,r6,r7":
-                return String.format("f(%s, 5)", mne);
-            case "$0x53525150,0x5b":
-                return String.format("lb(%s, 1)", mne);
-            case "$0x53525150,r4":
-                return String.format("l(%s, 2)", mne);
-            case "$0x53525150,r4,0x5d":
-                return String.format("lb(%s, 2)", mne);
-            case "$0x53525150,r4,r5":
-                return String.format("l(%s, 3)", mne);
-            case "$0x53525150,r4,r5,0x5760":
-                return String.format("lw(%s, 3)", mne);
-            case "$0x53525150,r4,r5,r6":
-                return String.format("l(%s, 4)", mne);
-            case "$0x53525150,r4,r5,r6,r7,r8":
-                return String.format("l(%s, 6)", mne);
-            case "$0x5756555453525150":
-                return String.format("q(%s, 1)", mne);
-            case "$0x5756555453525150 [d-float]":
-                return String.format("d(%s, 1)", mne);
-            case "$0x5756555453525150 [d-float],r8":
-                return String.format("d(%s, 2)", mne);
-            case "$0x5756555453525150 [d-float],r8,r9":
-                return String.format("d(%s, 3)", mne);
-            case "$0x5756555453525150 [d-float],r8,r9,0x5b68":
-                return String.format("dw(%s, 3)", mne);
-            case "$0x5756555453525150 [d-float],r8,r9,r10,r11":
-                return String.format("d(%s, 5)", mne);
-            case "$0x5756555453525150 [g-float]":
-                return String.format("g(%s, 1)", mne);
-            case "$0x5756555453525150 [g-float],r8":
-                return String.format("g(%s, 2)", mne);
-            case "$0x5756555453525150 [g-float],r8,r9":
-                return String.format("g(%s, 3)", mne);
-            case "$0x5756555453525150 [g-float],r8,r9,0x5b69":
-                return String.format("gw(%s, 3)", mne);
-            case "$0x5756555453525150 [g-float],r8,r9,r10,r11":
-                return String.format("g(%s, 5)", mne);
-            case "$0x5756555453525150,r8":
-                return String.format("q(%s, 2)", mne);
-            case "$0x5f5e5d5c5b5a59585756555453525150 [h-float]":
-                return String.format("h(%s, 1)", mne);
-            case "$0x5f5e5d5c5b5a59585756555453525150 [h-float],(r0)":
-                return String.format("h(%s, 2)", mne);
-            case "$0x5f5e5d5c5b5a59585756555453525150 [h-float],(r0),(r1)":
-                return String.format("h(%s, 3)", mne);
-            case "$0x5f5e5d5c5b5a59585756555453525150 [h-float],(r0),(r1),(r2),(r3)":
-                return String.format("h(%s, 5)", mne);
-            case "$0x5f5e5d5c5b5a59585756555453525150 [h-float],(r0),(r1),0x6379":
-                return String.format("hw(%s, 3)", mne);
-            case "$0x5f5e5d5c5b5a59585756555453525150,(r0)":
-                return String.format("o(%s, 2)", mne);
-            case "0x5092":
-                return String.format("brw(%s)", mne);
-            case "0xffffff91":
-                return String.format("brb(%s)", mne);
-            default:
-                return null;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         String[][] ops = new String[4][];
         for (int i = 0; i < 4; ++i) {
             ops[i] = new String[0x100];
         }
-        FileReader fr = new FileReader("research/allop.d");
-        BufferedReader br = new BufferedReader(fr);
-        String line;
-        while ((line = br.readLine()) != null) {
-            int p1 = line.indexOf('\t') + 1;
-            if (p1 > 0) {
-                int b = Integer.parseInt(line.substring(0, 2), 16);
-                int i = b < 0xfd ? 0 : b - 0xfc;
-                int j = b < 0xfd ? b : Integer.parseInt(line.substring(3, 5), 16);
-                ops[i][j] = convert(line, p1, b);
-            }
-        }
-        br.close();
-        fr.close();
-
-        System.out.println("{");
-        for (int i = 0; i < 4; ++i) {
-            String h = i == 0 ? "" : String.format("%02x", 0xfc + i);
-            System.out.println("    {");
-            for (int j = 0; j < 0x100; j += 8) {
-                System.out.printf("        /* %s%02x */", h, j);
-                for (int k = 0; k < 8; ++k) {
-                    String o = ops[i][j + k];
-                    System.out.printf(" %s,", o != null ? o : "null");
+        try (BufferedReader br = new BufferedReader(
+                new FileReader("research/allopr-t.txt"))) {
+            System.out.print("enum VAXOp {");
+            int prev = 0;
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] f = line.split("\t");
+                String f0 = f[0].replaceAll("-", "");
+                int op = Integer.parseInt(f0, 16);
+                if ((op & 3) == 0 || op >> 4 != prev >> 4) {
+                    System.out.println();
+                    System.out.print("   ");
                 }
-                System.out.println();
+                prev = op;
+                String t = f.length > 2 ? f[2] : "";
+                System.out.printf(" %s(0x%s, \"%s\"),", f[1].toUpperCase(), f0, t);
             }
-            System.out.println("    },");
+            System.out.println();
+            System.out.println("}");
         }
-        System.out.println("};");
     }
 }
