9b586fa91a0248c05d8d1f851129c60f5405dd4f
7shi <7shi@live.jp>
2016-04-12 10:16:29 +0900

    未使用のインポートを整理

--- src/vaxrun/Main.java
+++ src/vaxrun/Main.java
@@ -9,8 +9,6 @@ import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 class Symbol {
 
